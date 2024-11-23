a53d889f892ea1b94a5f7416e326772d989604e8
Christian Adler <chris.adler@gmx.de>
2024-11-23 07:29:00 +0100

    add gravity and show rest

--- main.mjs
+++ main.mjs
@@ -24,10 +24,11 @@ const updateWorldSettings = () => {
 updateWorldSettings();
 
 const anchor = new Vector(200, 0);
-let bob = new Vector(250, 250);
+let bob = new Vector(290, 250);
 let restLength = 200;
-let k = 0.001;
+let k = 0.002;
 let velocity = new Vector(0, 0);
+const gravity = new Vector(0, 0.1);
 
 const update = () => {
 
@@ -42,22 +43,23 @@ const update = () => {
 
   ctx.save();
 
+  ctx.beginPath();
+  ctx.moveTo(anchor.x, anchor.y);
+  ctx.lineTo(bob.x, bob.y);
+  ctx.stroke();
+
+  // rest (without gravity) would be
+  const rest = bob.clone().subVec(anchor).normalize().mult(restLength);
+  rest.addVec(anchor);
+  ctx.beginPath();
+  ctx.moveTo(anchor.x, anchor.y);
+  ctx.lineTo(rest.x, rest.y);
+  ctx.strokeStyle = "red";
+  ctx.stroke();
 
   ctx.beginPath();
   ctx.arc(bob.x, bob.y, 20, 0, Math.PI * 2);
   ctx.fill();
-
-
-  ctx.beginPath();
-  ctx.moveTo(anchor.x, anchor.y);
-  ctx.lineTo(anchor.x, anchor.y + restLength);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.moveTo(254, bob.y);
-  ctx.lineTo(254, restLength);
-  ctx.stroke();
-
 
   ctx.restore();
 
@@ -70,6 +72,7 @@ const update = () => {
   // F = M * A // we set M = 1;
   // F= A
   velocity.addVec(force);
+  velocity.addVec(gravity);
   bob.addVec(velocity);
 
   // damping
